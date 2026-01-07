8d67f34650127950ec154d2aa5033b6869b2e847
Ajay Bhatt <ajay.bhatt@algoworks.com>
2026-01-07 12:36:18 +0530

    changes

---
+++ ArrayProblems/remove_duplicates_from_array.py
@@ -0,0 +1,12 @@
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    write = 1   # position to write next unique element
+
+    for read in range(1, len(nums)):
+        if nums[read] != nums[read - 1]:
+            nums[write] = nums[read]
+            write += 1
+
+    return write
