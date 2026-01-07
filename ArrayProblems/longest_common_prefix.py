0df7507f636f0fae3dee8ec2e4f4584c3e307fbf
Ajay Bhatt <ajay.bhatt@algoworks.com>
2026-01-07 12:35:28 +0530

    longest common prefix

---
+++ ArrayProblems/longest_common_prefix.py
@@ -0,0 +1,10 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+
+    for i in range(len(strs[0])):
+        char = strs[0][i]
+        for s in strs:
+            if i >= len(s) or s[i] != char:
+                return strs[0][:i]
+    return strs[0]
--- ArrayProblems/sum_of_two_number.py
+++ ArrayProblems/sum_of_two_number.py
@@ -1,19 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}
+def twoSum(nums, target):
+    num_to_index = {}
 
-        # Iterate over the array
-        for i, num in enumerate(nums):
-            # Calculate the complement of the current number
-            complement = target - num
+    # Iterate over the array
+    for i, num in enumerate(nums):
+        # Calculate the complement of the current number
+        complement = target - num
 
-            # Check if the complement is already in the dictionary
-            if complement in num_to_index:
-                # If found, return the indices of the complement and the current number
-                return [num_to_index[complement], i]
+        # Check if the complement is already in the dictionary
+        if complement in num_to_index:
+            # If found, return the indices of the complement and the current number
+            return [num_to_index[complement], i]
 
-            # Store the current number and its index in the dictionary
-            num_to_index[num] = i
+        # Store the current number and its index in the dictionary
+        num_to_index[num] = i
 
-        # If no solution is found (though the problem states there will always be one)
-        return []
+    # If no solution is found (though the problem states there will always be one)
+    return []
