fcfc7dec13d977228886f90786a8d20ae36e72d2
Ajay Bhatt <ajay.bhatt@algoworks.com>
2026-01-07 12:57:15 +0530

    changes in remove duplicate script

--- ArrayProblems/remove_duplicates_from_array.py
+++ ArrayProblems/remove_duplicates_from_array.py
@@ -2,7 +2,7 @@ def removeDuplicates(nums):
     if not nums:
         return 0
 
-    write = 1   # position to write next unique element
+    write = 1  # position to write next unique element
 
     for read in range(1, len(nums)):
         if nums[read] != nums[read - 1]:
@@ -10,3 +10,10 @@ def removeDuplicates(nums):
             write += 1
 
     return write
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+k = removeDuplicates(nums)
+
+print(k)  # 5
+print(nums)
