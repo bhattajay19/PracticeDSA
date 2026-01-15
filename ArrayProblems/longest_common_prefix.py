457f9352b3bea7dd7f28b711c99c0f448163cf6f
Ajay Bhatt <ajay.bhatt@algoworks.com>
2026-01-15 11:36:56 +0530

    closest three sum problem

--- ArrayProblems/longest_common_prefix.py
+++ ArrayProblems/longest_common_prefix.py
@@ -1,10 +1,17 @@
 def longestCommonPrefix(strs):
     if not strs:
         return ""
+    common_prefix = strs[0]
+    for string in strs[1:]:
+        while not string.startswith(common_prefix):
+            common_prefix = common_prefix[:-1]
+    return common_prefix
+    # for i in range(len(strs[0])):
+    #     char = strs[0][i]
+    #     for s in strs:
+    #         if i >= len(s) or s[i] != char:
+    #             return strs[0][:i]
+    # return strs[0]
 
-    for i in range(len(strs[0])):
-        char = strs[0][i]
-        for s in strs:
-            if i >= len(s) or s[i] != char:
-                return strs[0][:i]
-    return strs[0]
+print(longestCommonPrefix(["flower","flow","flight"]))
+print(longestCommonPrefix(["dog","racecar","car"]))
