6d8660926edff971263463ff91520b6230103b21
Ajay Bhatt <ajay.bhatt@algoworks.com>
2026-01-21 11:17:36 +0530

    changes

--- ArrayProblems/search_is_rotated_array.py
+++ ArrayProblems/search_is_rotated_array.py
@@ -7,15 +7,15 @@ def search(nums, target):
             return mid
         if nums[left] <= nums[mid]:
 
-            if nums[left] <= target < nums[mid]:
+            if nums[left] <= target <= nums[mid]:
                 right = mid - 1
             else:
                 left = mid + 1
         else:
-            if nums[mid] <= target < nums[right]:
+            if nums[mid] <= target <= nums[right]:
                 left = mid + 1
             else:
                 right = mid - 1
     return -1
 
-print(search([4,5,6,7,0,1,2],0))
+print(search([5,1,3],3))
