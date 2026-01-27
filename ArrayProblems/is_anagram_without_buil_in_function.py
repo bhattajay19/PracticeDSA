e74abeef305e6568d3a147a3033b05c13d222a5a
Ajay Bhatt <ajay.bhatt@algoworks.com>
2026-01-27 15:47:56 +0530

    changes

--- ArrayProblems/is_anagram_without_buil_in_function.py
+++ ArrayProblems/is_anagram_without_buil_in_function.py
@@ -1,16 +1,19 @@
 def is_anagram(str1, str2):
     if len(str1) != len(str2):
         return False
-    str1 = str1.lower()
-    str2 = str2.lower()
 
     array = [0] * 256
     for char in str1:
+        print(char)
+        print(array)
         array[ord(char)] += 1
     for char in str2:
+        print(array)
         array[ord(char)] -= 1
 
     for arr in array:
-        if arr == 0:
+        if arr != 0:
             return False
     return True
+
+print(is_anagram('silent', 'listen'))
